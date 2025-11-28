9552b241e9124f6a32d41baf6135ce6714d03d17
Echowispp <kauri.sykari190@gmail.com>
2025-11-28 21:57:47 +0200

    changed parameters for more realism
    additional changes:
    - widened the sliders' range
    - fixed frequency values above 100%
    - many efficiency improvements, size of them ranges from very small to major
    - changed angleSlider name to gravitySlider

--- main.js
+++ main.js
@@ -1,20 +1,25 @@
-let area = null;
-
 const canvas = document.getElementById("mainCanvas");
 const ctx = canvas.getContext("2d");
 
-const angleSlider = document.getElementById("angleSlider");
-const angleValue = document.getElementById("angleValue");
+const gravitySlider = document.getElementById("gravitySlider");
+const gravityValue = document.getElementById("gravityValue");
 const frequencySlider = document.getElementById("frequencySlider");
 const frequencyValue = document.getElementById("frequencyValue");
 const speedSlider = document.getElementById("speedSlider");
 const speedValue = document.getElementById("speedValue");
 
-let moveAngle = 1;
-let frequency = 40; //in percent
-let speedMulti = 0.5;
+let frequency = 40;
+/*
+unit is in percent chance per frame
+for each 100% there is 1 guaranteed spawn/frame as well as the rest being a chance for an extra spawn
+*/
+let snowThickness = 40;
+let windSpeed = 0.5;
+let gravity = 0.5;
 
-snow = [];
+let moveAngle = Math.atan2(gravity, windSpeed);
+
+let snow = [];
 
 //================
 // CANVAS RESIZING
@@ -28,7 +33,7 @@ window.addEventListener("load", function () {
 window.addEventListener("resize", resizeCanvas);
 
 function resizeCanvas() {
-	area = canvas.getBoundingClientRect();
+	const area = canvas.getBoundingClientRect();
 	canvas.width = area.width;
 	canvas.height = area.height;
 }
@@ -37,24 +42,24 @@ function resizeCanvas() {
 //  SLIDERS
 //===========
 
-angleSlider.addEventListener("input", (e) => {
-	const degrees = e.target.value;
-	angleValue.textContent = parseFloat(degrees).toFixed(1);
-	moveAngle = degrees * (Math.PI / 180);
-	console.log("moveAngle: ", moveAngle, moveAngle / Math.PI);
+gravitySlider.addEventListener("input", (e) => {
+	const value = e.target.value;
+	gravityValue.textContent = parseFloat(value).toFixed(1);
+	gravity = parseFloat(value);
 });
+
 frequencySlider.addEventListener("input", (e) => {
 	const freq = e.target.value;
 	frequencyValue.textContent = parseFloat(freq).toFixed(1);
-	frequency = freq;
+	frequency = parseFloat(freq);
 });
 speedSlider.addEventListener("input", (e) => {
 	const spd = e.target.value;
 	speedValue.textContent = parseFloat(spd).toFixed(2);
-	speedMulti = spd;
+	windSpeed = parseFloat(spd);
 });
 function loadSliders() {
-	for (slider of document.querySelectorAll('input[type="range"]')) {
+	for (let slider of document.querySelectorAll('input[type="range"]')) {
 		slider.addEventListener("input", (e) => {
 			const slider = e.target;
 			const percent =
@@ -89,33 +94,46 @@ function animationLoop() {
 //===========
 
 function updateCanvas() {
-	updateSnow();
+	const moveSpeed = Math.sqrt(gravity * gravity + windSpeed * windSpeed);
 
-	const dx = Math.cos(moveAngle);
-	const dy = Math.sin(moveAngle);
+	updateSnow(moveSpeed);
 
 	ctx.strokeStyle = "#f0f1e7";
-
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	ctx.beginPath();
 
-	for (flake of snow) {
-		mvX = dx * flake.speed;
-		mvY = dy * flake.speed;
+	moveAngle = Math.atan2(gravity, windSpeed);
 
-		ctx.beginPath();
+	for (let flake of snow) {
+		const angleVariation = (Math.random() - 0.5) * 0.2;
+		const dx = Math.cos(moveAngle + angleVariation);
+		const dy = Math.sin(moveAngle + angleVariation);
+
 		ctx.moveTo(flake.x, flake.y);
-		ctx.lineTo(flake.x + mvX, flake.y + mvY);
-		ctx.stroke();
 
-		flake.x += mvX;
-		flake.y += mvY;
+		flake.x += dx * (1 + flake.speed);
+		flake.y += dy * (1 + flake.speed);
+
+		ctx.lineTo(flake.x, flake.y);
 	}
+
+	ctx.stroke();
 }
 
-function updateSnow() {
-	if (Math.floor(Math.random(0, 100)) + 1 < frequency) {
+//=================
+// SNOWFLAKE LOGIC
+//=================
+
+function updateSnow(moveSpeed) {
+	snowThickness = (frequency / 100) * moveSpeed * 8;
+
+	for (let i = 0; i < Math.floor(snowThickness); i++) {
 		createFlake();
 	}
+	if (Math.random() < snowThickness - Math.floor(snowThickness)) {
+		createFlake();
+	}
+
 	snow = snow.filter((flake) => {
 		const inYArea = flake.y > -25 && flake.y < canvas.height + 25;
 		const inXArea = flake.x > -25 && flake.x < canvas.width + 25;
@@ -126,7 +144,7 @@ function updateSnow() {
 
 function createFlake() {
 	const pos = () => {
-		elementaryPos = Math.random() * (canvas.width + canvas.height);
+		let elementaryPos = Math.random() * (canvas.width + canvas.height);
 		if (elementaryPos > canvas.width) {
 			if (moveAngle > Math.PI / 2) {
 				return [canvas.width, elementaryPos - canvas.width];
@@ -136,21 +154,14 @@ function createFlake() {
 		} else {
 			return [elementaryPos, 0];
 			/*  all this basically makes sure that the little bit in the bottom corner 
-                will be filled no matter the steepess of moveAngle
+                will be filled no matter how steep of a slope the movement is on
             */
 		}
 	};
 	const [x, y] = pos();
 
-	spd =
-		/*() => {
-		let elementrySpd = */ Math.random() * speedMulti; /*
-		if (elementrySpd < 0.1) {
-			return 0.1;
-		} else {
-			return elementrySpd;
-		}
-	};*/
+	const spd = 0.1 + Math.random() * windSpeed * 2; // on average, spd = windSpeed
+
 	snow.push({
 		x: x,
 		y: y,
